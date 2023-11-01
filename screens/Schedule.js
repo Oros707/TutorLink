077293addafdb7b1a561270f33621f4c4a255b10
xtla1298 <letsepethatoandrew@gmail.com>
2023-11-01 20:32:12 +0200

    firebase

--- screens/Schedule.js
+++ screens/Schedule.js
@@ -1,149 +1,53 @@
-import React, { useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { Picker } from '@react-native-picker/picker';
+import React, { useState, useEffect } from 'react';
+import { View, Text, FlatList } from 'react-native';
+import { db } from '../config/firebase'; // Import db from your Firebase configuration
 
 const Schedule = () => {
-  const [selectedYear, setSelectedYear] = useState('FirstYear');
-  const [selectedModuleCode, setSelectedModuleCode] = useState('');
+  const [timetableData, setTimetableData] = useState([]);
 
-  
-  const timetable = [
-    // Your timetable data here
-  ];
+  useEffect(() => {
+    const academicYear = "ThirdYear";
+    const dayOfWeek = "Friday";
+    const timeSlot = "10h00-12h00";
 
-  // Define module codes for each year
-  const moduleCodesByYear = {
-    FirstYear: ['BAY01B1', 'DSW01B1', 'SSW01B1', 'IFS01B1'],
-    SecondYear: ['BAY02B1', 'DSW02B1', 'SSW02B1', 'IFS02B1'],
-    ThirdYear: ['BAY03B1', 'DSW03B1', 'SSW03B1', 'IFS03B1'],
-  };
+    const retrieveTimetable = async () => {
+      try {
+        const timetableDocRef = db.collection('timetable')
+          .doc(academicYear)
+          .collection(dayOfWeek)
+          .doc(timeSlot);
 
-  // Update the module code options when the year selection changes
-  const handleYearChange = (year) => {
-    setSelectedYear(year);
-    setSelectedModuleCode('');
-  };
+        const doc = await timetableDocRef.get();
+        if (doc.exists) {
+          const data = doc.data();
+          setTimetableData([{ startTime: timeSlot, endTime: data.endTime, module: data.module }]);
+        } else {
+          console.error('No document found');
+        }
+      } catch (error) {
+        console.error('Error retrieving timetable:', error);
+      }
+    };
+
+    // Call the function when the component loads
+    retrieveTimetable();
+  }, []);
 
   return (
-    <View style={styles.container}>
-      <View>
-        <Text style={styles.title}> Tutoring Schedule</Text>
-      </View>
-      <View style={styles.dropdowns}>
-        <View style={styles.dropdownContainer}>
-          <Text style={styles.label}>Year of Study</Text>
-          <Picker
-            selectedValue={selectedYear}
-            onValueChange={(itemValue) => handleYearChange(itemValue)}
-            style={styles.dropdown}
-          >
-            <Picker.Item label="Select Year of Study" value="" />
-            <Picker.Item label="First Year" value="FirstYear" />
-            <Picker.Item label="Second Year" value="SecondYear" />
-            <Picker.Item label="Third Year" value="ThirdYear" />
-          </Picker>
-        </View>
-
-        <View style={styles.dropdownContainer}>
-          <Text style={styles.label}>Module Code</Text>
-          <Picker
-            selectedValue={selectedModuleCode}
-            onValueChange={(itemValue) => setSelectedModuleCode(itemValue)}
-            style={styles.dropdown}
-          >
-            <Picker.Item label="Select Module Code" value="" />
-            {selectedYear &&
-              moduleCodesByYear[selectedYear].map((code) => (
-                <Picker.Item key={code} label={code} value={code} />
-              ))}
-          </Picker>
-        </View>
-      </View>
-
-      {/* Add some spacing */}
-      <View style={{ marginBottom: 20 }}></View>
-
-      <View style={styles.headerRow}>
-        <Text style={styles.headerCell}>Time</Text>
-        <Text style={styles.headerCell}>Monday</Text>
-        <Text style={styles.headerCell}>Tuesday</Text>
-        <Text style={styles.headerCell}>Wednesday</Text>
-        <Text style={styles.headerCell}>Thursday</Text>
-        <Text style={styles.headerCell}>Friday</Text>
-      </View>
-
-      {timetable.map((rowData, index) => (
-        <View key={index} style={styles.dataRow}>
-          <Text style={styles.dataCell}>{rowData.time}</Text>
-          <Text style={styles.dataCell}>{rowData.Monday}</Text>
-          <Text style={styles.dataCell}>{rowData.Tuesday}</Text>
-          <Text style={styles.dataCell}>{rowData.Wednesday}</Text>
-          <Text style={styles.dataCell}>{rowData.Thursday}</Text>
-          <Text style={styles.dataCell}>{rowData.Friday}</Text>
-        </View>
-      ))}
+    <View>
+      <Text>Timetable for {academicYear} on {dayOfWeek} from {timeSlot}</Text>
+      <FlatList
+        data={timetableData}
+        keyExtractor={(item) => item.startTime}
+        renderItem={({ item }) => (
+          <View>
+            <Text>Time: {item.startTime} - {item.endTime}</Text>
+            <Text>Module: {item.module}</Text>
+          </View>
+        )}
+      />
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 16,
-    backgroundColor:'orange',
-    
-  },
-  dropdowns: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    marginBottom: 30,
-    marginTop:49,
-    
-  },
-  dropdownContainer: {
-    flex: 1,
-  },
-  dropdown: {
-    flex: 1,
-  },
-  label: {
-    fontSize: 16,
-    fontWeight: 'bold',
-    marginBottom: 4,
-  },
-  headerRow: {
-    flexDirection: 'row',
-    backgroundColor: '#f2f2f2',
-    padding: 8,
-    marginBottom: 8,
-    borderRadius: 8,
-  },
-  dataRow: {
-    flexDirection: 'row',
-    padding: 8,
-    marginBottom: 8,
-    borderRadius: 8,
-  },
-  headerCell: {
-    flex: 1,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    borderWidth: 1,
-    borderColor: 'black',
-  },
-  dataCell: {
-    flex: 1,
-    textAlign: 'center',
-    borderWidth: 1,
-    borderColor: 'black',
-  },
-  title:{
-    marginTop:30,
-    textAlign:'center',
-    fontSize:30,
-    color:'black',
-
-  }
-});
 
 export default Schedule;
