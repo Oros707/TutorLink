c4fb14925acdfe5265070e08ce4604cc05521bd2
Jimmy <nkgogojimmy@gmail.com>
2023-11-03 11:38:29 +0200

    Revert "Updated Claims,LoginScreen and Admin"

--- screens/ClaimProcess/ClaimDetails.js
+++ screens/ClaimProcess/ClaimDetails.js
@@ -1,112 +1,159 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet, KeyboardAvoidingView, Platform, ScrollView } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, KeyboardAvoidingView, Platform ,ScrollView} from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { useContext } from 'react';
 import DetailsContext from './DetailsContext';
-import ModalDropdown from 'react-native-modal-dropdown';
+
 
 export default function ClaimDetails() {
   const navigation = useNavigation();
-  const [contractETNo] = useState('');
-  const [totalUnits, setTotalUnits] = useState('');
-  const [name, setName] = useState('');
-  const [surname, setSurname] = useState('');
-  const [identity, setIdentity] = useState('');
-  const [gender, setGender] = useState('');
-  const [citizenship, setCitizenship] = useState('');
-  const [address, setAddress] = useState('');
-  const [email, setEmail] = useState('');
-  const [phonenumber, setPhoneNumber] = useState('');
-  const [disability, setDisability] = useState('');
-  const [race, setRace] = useState('');
-  const [centernumber, setCenterNumber] = useState('');
+  const [contactETNo] = useState(''); // Set the initial value for Contact E/T No.
+  const [totalUnits, setTotalUnits] = useState('0');
+  const [rate, setRate] = useState('0');
   const [currency] = useState('ZAR'); // Set the initial value for Currency
-  const [CentreNumber, setCostCentreNumber] = useState('');
-
+  const [costCentreNumber, setCostCentreNumber] = useState('');
+  const [total, setTotal] = useState('');
   const [details, setDetails] = useContext(DetailsContext);
 
+  // Function to calculate the amount
+
+
   const handleNext = () => {
-    setDetails((prev) => ({
+
+    setDetails(prev => ({
       ...prev,
       ClaimDetails: {
-        contractETNo,
-        name,
-        surname,
-        identity,
-        gender,
-        citizenship,
-        address,
-        email,
-        phonenumber,
-        disability,
-        race,
+        contactETNo,
+        totalUnits,
+        rate,
+        currency,
+        costCentreNumber,
+        total,
+        details,
       },
     }));
     // Navigate to BankingDetails
     navigation.navigate('BankingDetails');
   };
-
+  
   return (
     <ScrollView style={styles.container}>
-      <KeyboardAvoidingView style={styles.container} behavior={Platform.OS === 'ios' ? 'padding' : null}>
-        <Text style={styles.label}>Appointment</Text>
+    <KeyboardAvoidingView
+      style={styles.container}
+      behavior={Platform.OS === 'ios' ? 'padding' : null}
+    >
+      <Text style={styles.label}>Appointment</Text>
 
-        <Text style={styles.inputLabel}>Enter Full Name</Text>
-        <TextInput style={styles.input} placeholder="Enter Full Name" value={name} onChangeText={setName} />
+      <Text style={styles.inputLabel}>Enter Full Name</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Enter Full Name"
+        value={contactETNo} // Set the initial value
+        editable={false} // Make it uneditable
+      />
 
-        <Text style={styles.inputLabel}>Enter Surname</Text>
-        <TextInput style={styles.input} placeholder="Enter Surname" value={surname} onChangeText={setSurname} />
+<Text style={styles.inputLabel}>Enter Surname</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Enter Full Name"
+        value={contactETNo} // Set the initial value
+        editable={false} // Make it uneditable
+      />
 
-        <Text style={styles.inputLabel}>Identity/Passport No.</Text>
-        <TextInput style={styles.input} placeholder="Enter Identity/Passport No." value={identity} onChangeText={setIdentity} />
+<Text style={styles.inputLabel}>Identity/Passport No.</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Enter Full Name"
+        value={contactETNo} // Set the initial value
+        editable={false} // Make it uneditable
+      />
 
-        <Text style={styles.inputLabel}>Gender</Text>
-        <TextInput style={styles.input} placeholder="Enter Gender" value={gender} onChangeText={setGender} />
+<Text style={styles.inputLabel}>Gender</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Enter Full Name"
+        value={contactETNo} // Set the initial value
+        editable={false} // Make it uneditable
+      />
 
-        <Text style={styles.inputLabel}>Are you a South African Citizen?</Text>
-        <TextInput
-          style={styles.input}
-          placeholder="IF NO, SPECIFY NATIONALITY"
-          value={citizenship}
-          onChangeText={setCitizenship}
-        />
+<Text style={styles.inputLabel}>Are you a South African Citizenship?</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="IF NO, SPECIFY NATIONALITY"
+        value={contactETNo} // Set the initial value
+        
+      />
 
-        <Text style={styles.inputLabel}>Home Address</Text>
-        <TextInput style={styles.input} placeholder="Enter Home Address" value={address} onChangeText={setAddress} />
+<Text style={styles.inputLabel}>Home Address</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Enter Full Name"
+        value={contactETNo} // Set the initial value
+      />
 
-        <Text style={styles.inputLabel}>Email Address</Text>
-        <TextInput style={styles.input} placeholder="Enter Email Address" value={email} onChangeText={setEmail} />
+<Text style={styles.inputLabel}>Email Address</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Enter Full Name"
+        value={contactETNo} // Set the initial value
+        editable={false} // Make it uneditable
+      />
 
-        <Text style={styles.inputLabel}>Cellphone Number</Text>
-        <TextInput style={styles.input} placeholder="Enter Cellphone Number" value={phonenumber} onChangeText={setPhoneNumber} />
+<Text style={styles.inputLabel}>Cellphone Number</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Enter Full Name"
+        value={contactETNo} // Set the initial value
+        editable={false} // Make it uneditable
+      />
 
-        <Text style={styles.inputLabel}>Disability</Text>
-        <TextInput style={styles.input} placeholder="IF YES, STATE NATURE" value={disability} onChangeText={setDisability} />
+<Text style={styles.inputLabel}>Disability</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="IF YES, STATE NATURE"
+        value={contactETNo} // Set the initial value
+         // Make it uneditable
+      />
 
-        <Text style={styles.inputLabel}>Race</Text>
-        <View style={styles.input}>
-          <ModalDropdown
-            options={['African', 'Colored', 'Indian', 'White']}
-            defaultValue="Select Race"
-            onSelect={(index, value) => setRace(value)}
-          />
-        </View>
+<Text style={styles.inputLabel}>Race</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Enter Full Name"
+        value={contactETNo} // Set the initial value
+        editable={false} // Make it uneditable
+      />
 
-        <Text style={styles.inputLabel}>Contract E/T No.</Text>
-        <TextInput style={styles.input} placeholder="Enter Contract E/T No." value={contractETNo} />
+      <Text style={styles.inputLabel}>Contract E/T No.</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Enter Contact E/T No."
+        value={contactETNo} // Set the initial value
+        editable={false} // Make it uneditable
+      />
 
-        <Text style={styles.inputLabel}>Employed at UJ</Text>
-        <TextInput
-          style={styles.input}
-          placeholder="Enter Employment Status"
-          value={centernumber}
-          onChangeText={(text) => setCenterNumber(text)}
-        />
+<Text style={styles.inputLabel}>Employed at UJ</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Enter Contact E/T No."
+        value={contactETNo} // Set the initial value
+        editable={false} // Make it uneditable
+      />
 
-        <TouchableOpacity style={styles.button} onPress={handleNext}>
-          <Text style={styles.buttonText}>Next</Text>
-        </TouchableOpacity>
-      </KeyboardAvoidingView>
+      <Text style={styles.inputLabel}>Cost Centre Number</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Enter Cost Centre Number"
+        value={costCentreNumber}
+        onChangeText={(text) => setCostCentreNumber(text)}
+      />
+
+      
+
+      <TouchableOpacity style={styles.button} onPress={handleNext}>
+        <Text style={styles.buttonText}>Next</Text>
+      </TouchableOpacity>
+      
+    </KeyboardAvoidingView>
     </ScrollView>
   );
 }
@@ -148,4 +195,3 @@ const styles = StyleSheet.create({
     fontSize: 18,
   },
 });
-
